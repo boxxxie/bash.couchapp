b3789808960c16d60b474fbfa537861ee3235c3c
Paul Iannazzo <somethingitalian@gmail.com>
2012-10-09 22:20:07 -0400

    added way to get doc revisions without using get (just head) added md5sums and base64 calcs

--- couchdb-bash.sh
+++ couchdb-bash.sh
@@ -12,6 +12,20 @@ couch-get() {
     curl -sX GET "$url"
 }
 
+couch-head(){
+    db="$1"
+    url="$host/$db"
+    echo curl -sI HEAD "$url"
+    curl -sI HEAD "$url"
+}
+
+couch-revision(){
+    db="$1"
+    url="$host/$db"
+    etag_kv=`curl -sI "$url" | grep ETag`
+    echo ${etag_kv:5}
+}
+
 couch-push(){
     http="$1"
     db="$2"
@@ -36,16 +50,18 @@ couch-put() {
 couch-upload() {
     db="$1"
     file="$2"
-    couch_response=`$(couch-get "$db")`
-    echo "response = $couch_response"
-    echo underscore -d "$couch_response" extract _rev
-    rev=$(underscore -d "$couch_response" extract _rev)
+#    couch_response=`$(couch-get "$db")`
+#    echo "response = $couch_response"
+#    echo underscore -d "$couch_response" extract _rev
+#    rev=$(underscore -d "$couch_response" extract _rev)
+    rev=`couch-revision "$db"`
     echo "rev = $rev"
     echo "rev w/o quotes = ${rev//\"}"
     safe_file=${file// /-}
     echo "file name = $safe_file"
-    url="$host/$1/$safe_file?rev=${rev//\"}"
+    url="$host/$db/$safe_file?rev=${rev//\"}"
     echo "$url"
+    #echo curl -sX PUT "$url" -H "Content-Type: $3" --data-binary @"$file"
     curl -sX PUT "$url" -H "Content-Type: $3" --data-binary @"$file"
 }
 
@@ -56,9 +72,19 @@ couch-upload() {
 dir_to_upload="$2"
 db="$3"
 
+#echo `couch-head "$db"`
+#echo `couch-revision "$db"`
+
+#exit 0
+
 cd "$dir_to_upload"
-find .  | while read file; do 
-    echo "$file"; 
+file_index=0
+find .  | while read file_long_name; do
+    #echo "$file"
+    file=${file_long_name:2}
+    blobs[file_index]=`base64 <"$file"`
+    md5s[file_index]=`md5deep <"$file"`
+    file_index=$[file_index+1]
     couch-upload "$db" "$file"
 done
 cd -
