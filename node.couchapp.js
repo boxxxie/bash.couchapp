7466eb779074896c7f316264a28599886457fbdf
Paul Iannazzo <somethingitalian@gmail.com>
2012-10-10 17:04:52 -0400

    trying to figure out how to process 23k files in nodejs or bash or anything!

--- node.couchapp.js
+++ node.couchapp.js
@@ -1,7 +1,14 @@
+var args = require('optimist')
+  .default('dir', __dirname)
+  .default('ouput','dump')
+  .argv;
+
+var async = require('async');
 var sys = require('sys')
 var exec = require('child_process').exec;
 var execFile = require('child_process').execFile;
 var _ = require('underscore');
+require('underscore_extended');
 var fs = require('fs');
 var nano = require('nano')("http://localhost:5984");
 var Q = require('q');
@@ -20,22 +27,46 @@ function get_host_name(){
   })
   return deferred.promise;
 }
-
+/*
 get_host_name()
   .then(function(){
     console.log(arguments);
   });
-
+*/
 var qexec = Q.nbind(exec);
+//var qcd = Q.nbind(process.chdir);
 
+/*
 qexec('hostname')
   .then(console.log);
 
-qexec('ls')
-  .then(function(){
-    return qexec('cat node.couchapp.js')
+qexec('find', '.' ,'-type f')
+  .then(function(file_paths)
+        {
+          console.log("files");
+          console.log(arguments);
+          _.each(file_paths,function(){
+            console.log(arguments);
+          })
+            })
+  .fail(function(){
+    console.log('failed')
   })
-  .then(function(){
-    return qexec('find .')
-  })
-  .then(console.log)
+*/
+
+
+execFile('./list_files',args.dir, function(err,file_paths){
+  var files = file_paths.split('/n');
+  console.log(file_paths)
+  console.log('error',err);
+  async.map(files,
+            function(path,done){
+              var nice_path = path.replace(/^\.\//,"");
+              done(null,nice_path);
+            },
+            function(err,nice_paths){
+              console.log(_.size(nice_paths));
+            })
+})
+
+
