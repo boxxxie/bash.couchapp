6f07c7c6c3bded004064ea042ba03f2deb50dae9
Paul Iannazzo <somethingitalian@gmail.com>
2012-10-23 00:37:28 -0400

    added dir-to-couchdb-json and couch-upload-dir functions for uploading bulk base64 chucnks to couchdb docs

--- couchdb-bash.sh
+++ couchdb-bash.sh
@@ -73,19 +73,18 @@ couch-upload-file() {
     echo "file name = $file_path"
     local attachment_url="${db_url}/${file_path}?rev=${rev_no_quotes}"
     echo "$attachment_url"
-    curl -X PUT "${attachment_url}" -H "Content-Type: ${mime}" --data-binary "@${file_path}"
+    curl -X PUT "${attachment_url}" -H "Content-Type: ${mime}" --data-binary "'" "@${file_path}" "'"
 }
 
-couch-upload-json() {
+couch-upload-json-file() {
     local db_url="$1"
     local json="$2"
-    local rev=$(doc-revision "$db_ur"l)
+    local rev=$(doc-revision "$db_url")
     rev=$(doc-revision "$db_url")
     echo "rev = $rev"
     local rev_no_quotes=$(trim "${rev//\"}")
     local attachment_url="${db_url}/${file_path}?rev=${rev_no_quotes}"
-    echo "$attachment_url"
-    curl -X PUT "${attachment_url}" -H "Content-Type: ${mime}" "${json}"
+    curl -v -X PUT "${attachment_url}" -H "Content-Type: application/json" --data-binary  "@${json}"
 }
 
 couch-upload-dir() {
@@ -100,34 +99,92 @@ couch-upload-dir() {
     cd -
 }
 
-couch-upload-dir-bulk() {
+base64_attachment() {
+    local file="$1";
+    local file_name="${file:2}";
+    local mime_type=$(xdg-mime query filetype "$file");
+    local file_base64=$(base64 -w 0 "$file");
+    echo '"'"$file_name"'"' : \
+        {'"content_type"' : '"'"$mime_type"'"' \
+        ,'"data"' : '"'"$file_base64"'"' }
+}
+
+md5_maker() {
+    local file="$1";
+    local file_name="${file:2}";
+    local md5=$(echo "$file" | md5deep) ;
+    echo '"'"$file_name"'"' : { '"md5"' : '"'"$md5"'"' }
+}
+
+dir-to-couchdb-json() {
     local url="$1"
     local upload_dir="$2"
     local file_dump="$3"
     cd "$upload_dir"
     doc=$(couch-get "$url")
-    #echo "$doc" | underscore print --color
 
-    echo {'"'_attachments'"' : { >> "$file_dump"
-    find . -type f | while read file; do 
-        local mime_type=$(xdg-mime query filetype "$file")
-        local file_base64=$(base64 -w 0 "$file")
-        
-        echo \
-            '"'"${file:2}"'"' : \
-            {'"contet_type"' : '"'"$mime_type"'"' \
-            ,'"data"' : '"'"$file_base64"'"' } , \
-         >> "$file_dump"
-    done
-    echo }} >> "$file_dump"
+    #get an array of all of the files we are going to upload to couchdb
+    #mapfile -t files < <(find . -type f) ;
+    
+#attachments removed
+    local doc_no_attachments=$(underscore extend --data "$doc" '{"_attachments": undefined}')
+#attachments extracted
+    local original_attachments=$(echo "$doc" | underscore extract _attachments)
 
-#working
-#    new_attachments=$(cat "$file_dump")
-#    cat "$file_dump"| underscore print --color
-#    echo "$new_attachments" | underscore print --color 
+    echo
+    echo original_attachments    
+    echo "$original_attachments"
 
-# not working
-#    echo "$new_attachments" | underscore extend "$doc" |  underscore print --color 
+    local attachment_md5s=$(underscore pluck --data "$original_attachments" md5)
+    echo
+    echo attachment_md5s    
+    echo "$attachment_md5s"
 
+    local original_attachments_trimmed_back="${original_attachments%'}'}"
+    local original_attachments_trimmed="${original_attachments_trimmed_back#'{'}"
+
+    doc_no_attachments_len="${doc_no_attachments}"
+    doc_no_attachments_beginning="${doc_no_attachments%'}'} ,"
+
+    echo "$doc_no_attachments_beginning" >> "$file_dump"
+
+
+#### md5 processing #####
+    
+    echo '"'"attachments_md5s"'"' ': {' >> "$file_dump" ;
+
+    local first_md5='';
+    find . -type f | {
+        while read file ; do          
+            if [ "$first_md5" ] ;
+            then
+                local md5=$(md5_maker "$file") ;
+                echo "$md5" ',' >> "$file_dump" ;
+            else
+                first_md5=$(md5_maker "$file") ;
+            fi
+        done
+        echo "$first_md5" '},' >> "$file_dump"
+    }
+### attachment processing ####
+
+    echo '"'"_attachments"'"' ': {' >> "$file_dump" ;
+    echo "$original_attachments_trimmed" ','  >> "$file_dump" ;
+
+    local first_attachment='';
+    find . -type f | {
+        while read file ; do          
+            if [ "$first_attachment" ] ;
+            then
+                local attachment=$(base64_attachment "$file") ;
+                echo "$attachment" ',' >> "$file_dump" ;
+            else
+                first_attachment=$(base64_attachment "$file") ;
+            fi
+        done
+        echo "$first_attachment" '}' >> "$file_dump"
+    }
+    echo '}' >> "$file_dump" ;
     cd -
 }
+
